4bf4f4e4e59ed873a5e98cff6bf1c5a19568286c
Chyanne Haugen <chyhaugen@gmail.com>
2020-04-02 11:56:28 -0700

    refactoring

---
+++ file.py
@@ -0,0 +1,33 @@
+import os
+
+class File(object):
+
+    #path to file with data for lights
+    filePath = " "
+
+    #to see if file has new data
+    timeLastUpdated = 0
+
+    #data found in file
+    fileData = " "
+
+    def __init__(self, pathTofile):
+        self.filePath = pathTofile
+
+    def checkIfUpdated(self):
+        try:
+            currTime = os.path.getmtime(self.filePath)
+
+            if currTime > self.timeLastUpdated:
+                self.updateData()
+                self.timeLastUpdated = currTime
+        except:
+            pass
+
+    def updateData(self):
+
+        with open(self.filePath) as f:
+            data = f.readline()
+
+
+
