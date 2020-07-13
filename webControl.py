3c97870aa0cb729799d1f06ff405936ca0865d16
Chyanne Haugen <chyhaugen@gmail.com>
2020-07-13 12:06:37 -0700

    change port number

--- webControl.py
+++ webControl.py
@@ -35,4 +35,4 @@ def Main():
     return render_template('homePage.html')
    
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5050, threaded=True)
+    app.run(host='0.0.0.0', port=5055, threaded=True)
